5ac64c88d7adb1b3fdd9579ad3bd08e6efac8b12
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2018-04-28 11:39:50 +0300

    Change account controller test

--- twitter-emulation-spring-mvc-java/src/test/java/acme/twitter/web/AccountControllerTest.java
+++ twitter-emulation-spring-mvc-java/src/test/java/acme/twitter/web/AccountControllerTest.java
@@ -1,22 +1,22 @@
 package acme.twitter.web;
 
 import acme.twitter.data.AccountRepository;
-import acme.twitter.data.JdbcAccountRepository;
 import org.junit.Test;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 public class AccountControllerTest {
     @Test
-    public void testHomePage() throws Exception {
-        //TODO: change repository to mock
-        AccountRepository accountRepository = new JdbcAccountRepository();
+    public void testLoginPage() throws Exception {
+        AccountRepository accountRepository = mock(AccountRepository.class);
         AccountController controller = new AccountController(accountRepository);
         MockMvc mockMvc = standaloneSetup(controller).build();
-        mockMvc.perform(get("/"))
-                .andExpect(view().name("login"));
+
+        mockMvc.perform(get("/account/login"))
+                .andExpect(view().name("loginForm"));
     }
 }
