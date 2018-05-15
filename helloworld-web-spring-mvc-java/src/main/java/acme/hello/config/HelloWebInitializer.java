9e9767868a4f99b18827ab8dfcbf9a7add5ab3da
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2018-05-15 17:37:44 +0300

    Add module

---
+++ helloworld-web-spring-mvc-java/src/main/java/acme/hello/config/HelloWebInitializer.java
@@ -0,0 +1,21 @@
+package acme.hello.config;
+
+import acme.hello.web.WebConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class HelloWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{RootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
