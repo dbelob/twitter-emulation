34c943e86e2e7aa9613e9c28098f7d56b4d9c7bc
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2018-08-07 20:22:35 +0300

    Add TestContainers using

---
+++ twitter-emulation-spring-boot/src/test/java/acme/twitter/OracleContainerTest.java
@@ -0,0 +1,36 @@
+package acme.twitter;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.junit.Rule;
+import org.junit.Test;
+import org.testcontainers.containers.OracleContainer;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class OracleContainerTest {
+    @Rule
+    public OracleContainer oracle = new OracleContainer();
+
+    @Test
+    public void testSimple() throws SQLException {
+        //TODO: implement
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(oracle.getJdbcUrl());
+        hikariConfig.setUsername(oracle.getUsername());
+        hikariConfig.setPassword(oracle.getPassword());
+
+        HikariDataSource ds = new HikariDataSource(hikariConfig);
+        Statement statement = ds.getConnection().createStatement();
+        statement.execute("SELECT 1 FROM dual");
+        ResultSet resultSet = statement.getResultSet();
+
+        resultSet.next();
+        int resultSetInt = resultSet.getInt(1);
+        assertEquals("A basic SELECT query succeeds", 1, resultSetInt);
+    }
+}
