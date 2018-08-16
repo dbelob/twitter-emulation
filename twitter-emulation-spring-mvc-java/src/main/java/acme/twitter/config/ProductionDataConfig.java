a2e360f13cfb5b44e5eb1135d822c61dc2e11f6d
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2018-08-16 17:31:48 +0300

    Support JNDI

--- twitter-emulation-spring-mvc-java/src/main/java/acme/twitter/config/ProductionDataConfig.java
+++ twitter-emulation-spring-mvc-java/src/main/java/acme/twitter/config/ProductionDataConfig.java
@@ -1,26 +1,21 @@
 package acme.twitter.config;
 
-import oracle.jdbc.pool.OracleDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 
 import javax.sql.DataSource;
-import java.sql.SQLException;
 
 @Configuration
 @Profile("production")
 public class ProductionDataConfig {
     @Bean
-    public DataSource dataSource() throws SQLException {
-        //TODO: change
-        OracleDataSource dataSource = new OracleDataSource();
-        dataSource.setUser("te");
-        dataSource.setPassword("password");
-        dataSource.setURL("jdbc:oracle:thin:@acme.com:1521:sid");
+    public DataSource dataSource() {
+        JndiDataSourceLookup dataSourceLookup = new JndiDataSourceLookup();
 
-        return dataSource;
+        return dataSourceLookup.getDataSource("jdbc/TeDataSource");
     }
 
     @Bean
