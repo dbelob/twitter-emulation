1870c206ef0613fb0a377778fa3ffdfafbe57e8a
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2018-08-24 21:18:02 +0300

    Add DAO tests

---
+++ twitter-emulation-common/src/test/java/acme/twitter/dao/FollowerDaoTest.java
@@ -0,0 +1,45 @@
+package acme.twitter.dao;
+
+import org.junit.Test;
+
+/**
+ * Follower DAO test.
+ */
+public class FollowerDaoTest {
+    protected static FollowerDao followerDao;
+
+    @Test
+    public void countFollowingByUsername() {
+        //TODO: implement
+    }
+
+    @Test
+    public void countFollowersByUsername() {
+        //TODO: implement
+    }
+
+    @Test
+    public void isExist() {
+        //TODO: implement
+    }
+
+    @Test
+    public void add() {
+        //TODO: implement
+    }
+
+    @Test
+    public void delete() {
+        //TODO: implement
+    }
+
+    @Test
+    public void findFollowingByUsername() {
+        //TODO: implement
+    }
+
+    @Test
+    public void findFollowersByUsername() {
+        //TODO: implement
+    }
+}
---
+++ twitter-emulation-common/src/test/java/acme/twitter/dao/TweetDaoTest.java
@@ -0,0 +1,35 @@
+package acme.twitter.dao;
+
+import org.junit.Test;
+
+/**
+ * Tweet DAO test.
+ */
+public class TweetDaoTest {
+    protected static TweetDao tweetDao;
+
+    @Test
+    public void findByUsername() {
+        //TODO: implement
+    }
+
+    @Test
+    public void findAllByUsername() {
+        //TODO: implement
+    }
+
+    @Test
+    public void add() {
+        //TODO: implement
+    }
+
+    @Test
+    public void deleteAll() {
+        //TODO: implement
+    }
+
+    @Test
+    public void countByUsername() {
+        //TODO: implement
+    }
+}
