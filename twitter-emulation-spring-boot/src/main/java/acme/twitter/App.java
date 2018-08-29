7998cd7fed5a000136469f864afa0d76a54d2703
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2018-08-29 13:46:37 +0300

    Use JNDI for Spring Boot

--- twitter-emulation-spring-boot/src/main/java/acme/twitter/App.java
+++ twitter-emulation-spring-boot/src/main/java/acme/twitter/App.java
@@ -1,12 +1,20 @@
 package acme.twitter;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.tomcat.util.descriptor.web.ContextResource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.MessageSourceAccessor;
-import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jndi.JndiObjectFactoryBean;
+
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class App extends SpringBootServletInitializer {
@@ -17,5 +25,44 @@ public class App extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    @Profile("production")
+    public TomcatServletWebServerFactory tomcatFactory() {
+        return new TomcatServletWebServerFactory() {
+            @Override
+            protected TomcatWebServer getTomcatWebServer(Tomcat tomcat) {
+                tomcat.enableNaming();
+
+                return super.getTomcatWebServer(tomcat);
+            }
+
+            @Override
+            protected void postProcessContext(Context context) {
+                ContextResource resource = new ContextResource();
+                resource.setName("jdbc/TeDataSource");
+                resource.setType(DataSource.class.getName());
+                resource.setProperty("driverClassName", "oracle.jdbc.pool.OracleDataSource");
+                resource.setProperty("driverType", "thin");
+                resource.setProperty("URL", "jdbc:oracle:thin:@acme.com:1521:sid");
+                resource.setProperty("user", "te");
+                resource.setProperty("password", "password");
+
+                context.getNamingResources().addResource(resource);
+            }
+        };
+    }
+
+    @Bean(destroyMethod = "")
+    @Profile("production")
+    public DataSource jndiDataSource() throws IllegalArgumentException, NamingException {
+        JndiObjectFactoryBean bean = new JndiObjectFactoryBean();
+        bean.setJndiName("java:comp/env/jdbc/TeDataSource");
+        bean.setProxyInterface(DataSource.class);
+        bean.setLookupOnStartup(false);
+        bean.afterPropertiesSet();
+
+        return (DataSource) bean.getObject();
     }
 }
