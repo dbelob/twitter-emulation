77949b2873ba2d0cf06bc01af2fd582ef5fcb218
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2018-08-30 00:18:34 +0300

    Create data source for Spring Boot

---
+++ twitter-emulation-spring-boot/src/main/java/acme/twitter/config/ProductionDataConfig.java
@@ -0,0 +1,53 @@
+package acme.twitter.config;
+
+import oracle.jdbc.pool.OracleDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import javax.validation.constraints.NotNull;
+import java.sql.SQLException;
+
+@Configuration
+@Profile("production")
+@ConfigurationProperties("oracle")
+public class ProductionDataConfig {
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private String url;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Bean
+    DataSource dataSource() throws SQLException {
+        OracleDataSource dataSource = new OracleDataSource();
+        dataSource.setUser(username);
+        dataSource.setPassword(password);
+        dataSource.setURL(url);
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
