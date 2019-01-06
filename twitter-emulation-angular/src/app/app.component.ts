c523e9a6f3bdbb3dadc0d3ea8ea2354074b694b5
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-01-06 23:50:11 +0300

    Change root component

--- twitter-emulation-angular/src/app/app.component.ts
+++ twitter-emulation-angular/src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  template: `<div class="bg-success p-2 text-center text-white">
+    This is Twitter
+    </div>`
 })
 export class AppComponent {
   title = 'twitter-emulation-angular';
