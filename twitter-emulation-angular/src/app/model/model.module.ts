537192b009e908d7211fbc98891f84a280bfba26
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-01-11 13:02:39 +0300

    Use REST web service

--- twitter-emulation-angular/src/app/model/model.module.ts
+++ twitter-emulation-angular/src/app/model/model.module.ts
@@ -1,9 +1,14 @@
 import {NgModule} from '@angular/core';
 import {TweetRepository} from './tweet.repository';
 import {StaticDataSource} from './static.datasource';
+import {RestDataSource} from './rest.datasource';
+import {HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
-  providers: [TweetRepository, StaticDataSource]
+  imports: [HttpClientModule],
+  providers: [TweetRepository,
+    {provide: StaticDataSource, useClass: RestDataSource}]
 })
 export class ModelModule {
 }
