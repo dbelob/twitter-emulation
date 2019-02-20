7f8a479b0e53847c6d4830b49de1c3bf18c1069a
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-02-20 23:56:08 +0300

    Delete unused

--- twitter-emulation-spring-boot-angular-web/src/app/app.component.ts
+++ twitter-emulation-spring-boot-angular-web/src/app/app.component.ts
@@ -2,7 +2,6 @@ import {Component} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Router} from '@angular/router';
 import {AppService} from './app.service';
-import 'rxjs/add/operator/finally';
 import {finalize} from "rxjs/operators";
 
 @Component({
@@ -11,9 +10,6 @@ import {finalize} from "rxjs/operators";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Demo';
-  data = {};
-
   constructor(private app: AppService, private http: HttpClient, private router: Router) {
     this.app.authenticate(undefined, undefined);
   }
