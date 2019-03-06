18c0905a770f9645c064e1d290bd3eed48e2dfc1
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-03-06 23:22:15 +0300

    Change import

--- twitter-emulation-spring-boot-jsp/src/main/java/acme/twitter/config/WebConfig.java
+++ twitter-emulation-spring-boot-jsp/src/main/java/acme/twitter/config/WebConfig.java
@@ -1,12 +1,14 @@
 package acme.twitter.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
