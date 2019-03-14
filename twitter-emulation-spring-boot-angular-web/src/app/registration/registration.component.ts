33c75ee6b5c034330fe9d2a801f4b354c07167f9
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-03-14 23:11:18 +0300

    Show validation error message

--- twitter-emulation-spring-boot-angular-web/src/app/registration/registration.component.ts
+++ twitter-emulation-spring-boot-angular-web/src/app/registration/registration.component.ts
@@ -30,4 +30,41 @@ export class RegistrationComponent implements OnInit {
       this.formSubmitted = false;
     }
   }
+
+  getValidationMessages(state: any, thingName?: string) {
+    let thing: string = state.path || thingName;
+    let messages: string[] = [];
+
+    if (state.errors) {
+      for (let errorName in state.errors) {
+        switch (errorName) {
+          case "required":
+            messages.push(`You must enter a ${thing}`);
+            break;
+          case "minlength":
+            messages.push(`A ${thing} must be at least ${state.errors['minlength'].requiredLength} characters`);
+            break;
+          case "maxlength":
+            messages.push(`A ${thing} must be no more than ${state.errors['maxlength'].requiredLength} characters`);
+            break;
+          case "pattern":
+            messages.push(`The ${thing} contains illegal characters`);
+            break;
+        }
+      }
+    }
+
+    return messages;
+  }
+
+  getFormValidationMessages(form: NgForm): string[] {
+    let messages: string[] = [];
+
+    Object.keys(form.controls).forEach(k => {
+      this.getValidationMessages(form.controls[k], k)
+        .forEach(m => messages.push(m));
+    });
+
+    return messages;
+  }
 }
