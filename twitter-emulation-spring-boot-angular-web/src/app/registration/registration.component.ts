da2f4839031e29cae52725f8ee5afb6f6f8c48d2
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-03-14 20:26:50 +0300

    Show validation error message

--- twitter-emulation-spring-boot-angular-web/src/app/registration/registration.component.ts
+++ twitter-emulation-spring-boot-angular-web/src/app/registration/registration.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Account } from "../account";
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html'
 })
 export class RegistrationComponent implements OnInit {
+  formSubmitted: boolean = false;
   newAccount: Account = new Account();
 
   constructor() {
@@ -17,5 +19,15 @@ export class RegistrationComponent implements OnInit {
   register(account: Account) {
     //TODO: implement
     console.log("New Account: " + JSON.stringify(account));
+  }
+
+  submitForm(form: NgForm) {
+    this.formSubmitted = true;
+    if (form.valid) {
+      this.register(this.newAccount);
+      this.newAccount = new Account();
+      form.reset();
+      this.formSubmitted = false;
+    }
   }
 }
