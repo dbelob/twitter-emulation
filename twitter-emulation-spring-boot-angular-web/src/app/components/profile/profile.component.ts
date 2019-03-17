6ce7a7b4dd1d956aca05ddf0331dae57d2331412
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-03-17 23:04:24 +0300

    Add validation to profile

--- twitter-emulation-spring-boot-angular-web/src/app/components/profile/profile.component.ts
+++ twitter-emulation-spring-boot-angular-web/src/app/components/profile/profile.component.ts
@@ -1,19 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
+import { ValidationService } from "../../services/validation.service";
+import { Account } from "../../models/account.model";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html'
 })
 export class ProfileComponent implements OnInit {
-  credentials = {username: '', password: '', confirmation: '', description: ''};
+  formSubmitted: boolean = false;
+  oldAccount: Account = new Account();
 
-  constructor() {
+  constructor(private validation: ValidationService) {
   }
 
   ngOnInit() {
   }
 
-  save() {
+  save(account: Account) {
     //TODO: implement
+  }
+
+  submitForm(form: NgForm) {
+    this.formSubmitted = true;
+    if (form.valid) {
+      this.save(this.oldAccount);
+      this.oldAccount = new Account();
+      form.reset();
+      this.formSubmitted = false;
+    }
+  }
+
+  getFormValidationMessages(form: NgForm): string[] {
+    return this.validation.getFormValidationMessages(form);
   }
 }
