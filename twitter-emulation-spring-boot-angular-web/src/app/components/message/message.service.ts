74c4a17d615e826a40631656e8e7d108d8625cab
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-03-22 12:57:26 +0300

    Handle errors

--- twitter-emulation-spring-boot-angular-web/src/app/components/message/message.service.ts
+++ twitter-emulation-spring-boot-angular-web/src/app/components/message/message.service.ts
@@ -5,6 +5,11 @@ import { Observable, Subject } from "rxjs";
 @Injectable()
 export class MessageService {
   private subject = new Subject<Message>();
+  private exceptionMessages = new Map<string, string>();
+
+  constructor() {
+    this.exceptionMessages.set('AccountExistsException', 'Account with the same name already exists');
+  }
 
   reportMessage(msg: Message) {
     this.subject.next(msg);
@@ -12,5 +17,23 @@ export class MessageService {
 
   get messages(): Observable<Message> {
     return this.subject;
+  }
+
+  getMessageText(response: Response): string {
+    let error = response['error'];
+
+    if (error) {
+      let trace = error['trace'];
+
+      for (let key of Array.from(this.exceptionMessages.keys())) {
+        let value = this.exceptionMessages.get(key);
+
+        if (trace.indexOf(key) != -1) {
+          return value;
+        }
+      }
+    }
+
+    return `Network Error: ${response.statusText} (${response.status})`;
   }
 }
--- twitter-emulation-spring-boot-angular-web/src/app/services/account.service.ts
+++ twitter-emulation-spring-boot-angular-web/src/app/services/account.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { Observable, throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
 import { Account } from "../models/account.model";
+import { MessageService } from "../components/message/message.service";
 
 @Injectable({
   providedIn: 'root'
@@ -10,28 +11,14 @@ import { Account } from "../models/account.model";
 export class AccountService {
   baseUrl = 'api/account/';
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private messageService: MessageService) {
   }
 
   register(account: Account): Observable<Account> {
     return this.http.post<Account>(this.baseUrl + 'register', account)
       .pipe(
         catchError((response: Response) => {
-          // console.log(JSON.stringify(response));
-
-          let error = response['error'];
-          if (error) {
-            // console.log(JSON.stringify(error));
-
-            let trace = error['trace'];
-            // console.log(JSON.stringify(trace));
-
-            if (trace.indexOf('AccountExistsException') != -1) {
-              return throwError(`Account with the same name already exists`);
-            }
-          }
-
-          return throwError(`Network Error: ${response.statusText} (${response.status})`);
+          return throwError(this.messageService.getMessageText(response));
         })
       );
   }
