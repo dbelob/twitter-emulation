f7217738b0335af9ed589ed633bf1f4fb82afd02
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-04-07 18:23:23 +0300

    Set follow button visibility

--- twitter-emulation-spring-boot-angular-web/src/app/modules/home/account.component.ts
+++ twitter-emulation-spring-boot-angular-web/src/app/modules/home/account.component.ts
@@ -5,18 +5,22 @@ import { Tweet } from "../../models/tweet.model";
 import { AuthenticationService } from "../../services/authentication.service";
 import { AccountService } from "../../services/account.service";
 import { TweetService } from "../../services/tweet.service";
+import { User } from "../../models/user.model";
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html'
 })
 export class AccountComponent implements OnInit {
+  private user: User = new User();
   private account: Account = new Account();
   private accountStatistics: AccountStatistics = new AccountStatistics();
   private tweets: Tweet[] = [];
 
   constructor(private authenticationService: AuthenticationService, private accountService: AccountService, private tweetService: TweetService) {
     authenticationService.getUser().subscribe(user => {
+      this.user = user;
+
       accountService.getAccount(user.name).subscribe(data => {
         this.account = data;
       });
--- twitter-emulation-spring-boot-angular-web/src/app/modules/home/follow.component.ts
+++ twitter-emulation-spring-boot-angular-web/src/app/modules/home/follow.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Account } from "../../models/account.model";
+import { User } from "../../models/user.model";
 
 @Component({
   selector: 'app-follow',
   templateUrl: './follow.component.html'
 })
 export class FollowComponent implements OnInit {
+  @Input('user')
+  user: User;
+
+  @Input('account')
+  account: Account;
+
+  private isFollowVisible = false;
   private copyrightDate = new Date();
 
   constructor() {
   }
 
   ngOnInit() {
+    this.isFollowVisible = this.user.name !== this.account.username;
   }
 
   follow() {
