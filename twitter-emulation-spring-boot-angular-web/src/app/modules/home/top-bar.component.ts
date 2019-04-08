31d07ed896216a2696a1c24f4672460e29490f48
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-04-08 12:15:23 +0300

    Submit search form

--- twitter-emulation-spring-boot-angular-web/src/app/modules/home/top-bar.component.ts
+++ twitter-emulation-spring-boot-angular-web/src/app/modules/home/top-bar.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from "../../services/authentication.service";
 import { Router } from "@angular/router";
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html'
 })
 export class TopBarComponent implements OnInit {
+  private searchText: string;
+
   constructor(private authenticationService: AuthenticationService, private router: Router) {
   }
 
@@ -23,8 +26,11 @@ export class TopBarComponent implements OnInit {
     });
   }
 
-  search() {
-    //TODO: implement
-    this.router.navigateByUrl('/account/search');
+  search(form: NgForm) {
+    this.router.navigate(['/account/search'], {
+      queryParams: {
+        "text": this.searchText
+      }
+    });
   }
 }
