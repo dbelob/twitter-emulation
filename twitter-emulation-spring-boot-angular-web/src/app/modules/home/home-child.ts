98e2901eaf4e19496d8441a6d0a68c637e5a3d81
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-05-18 22:50:05 +0300

    Support unauthenticated user

--- twitter-emulation-spring-boot-angular-web/src/app/modules/home/home-child.ts
+++ twitter-emulation-spring-boot-angular-web/src/app/modules/home/home-child.ts
@@ -1,11 +1,9 @@
 import { ActivatedRoute, Router } from "@angular/router";
 import { Observer } from "rxjs";
 import { AuthenticationService } from "../../shared/services/authentication.service";
-import { User } from "../../shared/models/user.model";
 import { UserState } from "../../shared/models/user-state.model";
 
 export abstract class HomeChild {
-  protected authenticatedUser: User = new User();
   protected title: string;
 
   protected constructor(private authenticationService: AuthenticationService, activatedRoute: ActivatedRoute, router: Router,
@@ -13,29 +11,38 @@ export abstract class HomeChild {
     activatedRoute.params.subscribe(params => {
       const selectedUserName = params['user'];
       const url = activatedRoute.snapshot.url;
-      console.log('selectedUserName: ' + selectedUserName + '; url: ' + url); //TODO: delete
 
-      this.authenticationService.getUser().subscribe(user => {
-        this.authenticatedUser = user;
-
-        if ((url[0].path === 'account') && (url[1].path === 'show')) {
-          if (!selectedUserName) {
-            router.navigate(['/account', 'show', this.authenticatedUser.name]);
-            return;
-          } else {
-            if (selectedUserName !== this.authenticatedUser.name) {
-              router.navigate(['/account', 'tweets', selectedUserName]);
-              return;
+      this.authenticationService.authenticate(undefined, () => {
+          this.authenticationService.getUser().subscribe(authenticatedUser => {
+            if ((url[0].path === 'account') && (url[1].path === 'show')) {
+              if (!selectedUserName) {
+                router.navigate(['/account', 'show', authenticatedUser.name]);
+                return;
+              } else {
+                if (selectedUserName !== authenticatedUser.name) {
+                  router.navigate(['/account', 'tweets', selectedUserName]);
+                  return;
+                }
+              }
             }
+
+            const userState = new UserState(authenticatedUser.name, selectedUserName);
+
+            userStateObserver.next(userState);
+            this.getData(userState.getDataUserName());
+          });
+        },
+        () => {
+          if ((url[0].path === 'account') && (url[1].path === 'show')) {
+            router.navigate(['/account', 'tweets', selectedUserName]);
+            return;
           }
-        }
 
-        const userState = new UserState(this.authenticatedUser.name, selectedUserName);
-        console.log('dataUserName: ' + userState.getDataUserName());  //TODO: delete
+          const userState = new UserState(undefined, selectedUserName);
 
-        userStateObserver.next(userState);
-        this.getData(userState.getDataUserName());
-      });
+          userStateObserver.next(userState);
+          this.getData(userState.getDataUserName());
+        }).subscribe();
     });
   }
 
