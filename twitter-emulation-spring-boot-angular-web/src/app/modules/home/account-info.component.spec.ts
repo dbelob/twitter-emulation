15ff0f8427485ff7918362d21f2847a8da658fde
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-06-25 21:23:12 +0300

    Change tests for Input property

--- twitter-emulation-spring-boot-angular-web/src/app/modules/home/account-info.component.spec.ts
+++ twitter-emulation-spring-boot-angular-web/src/app/modules/home/account-info.component.spec.ts
@@ -1,31 +1,57 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from "@angular/router/testing";
+import { Component, DebugElement, ViewChild } from "@angular/core";
+import { By } from "@angular/platform-browser";
 import { AccountInfoComponent } from './account-info.component';
-import { RouterModule } from "@angular/router";
-import { USER_STATE, UserState } from "../../shared/models/user-state.model";
-import { Subject } from "rxjs";
+import { AccountStatistics } from "../../shared/models/account-statistics.model";
+
+@Component({
+  template: `
+    <app-account-info [accountStatistics]="accountStatistics"></app-account-info>`
+})
+class TestComponent {
+  public accountStatistics: AccountStatistics = new AccountStatistics();
+
+  @ViewChild(AccountInfoComponent, {static: true})
+  accountInfoComponent: AccountInfoComponent;
+}
 
 describe('AccountInfoComponent', () => {
+  let fixture: ComponentFixture<TestComponent>;
   let component: AccountInfoComponent;
-  let fixture: ComponentFixture<AccountInfoComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AccountInfoComponent],
+      declarations: [AccountInfoComponent, TestComponent],
       imports: [
-        RouterModule
+        RouterTestingModule
       ],
-      providers: [{provide: USER_STATE, useValue: new Subject<UserState>()}]
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AccountInfoComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance.accountInfoComponent;
+    debugElement = fixture.debugElement.query(By.directive(AccountInfoComponent));
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('receives the account statistics through an input property', () => {
+    expect(component.accountStatistics.username).toBeUndefined();
+    expect(component.accountStatistics.description).toBeUndefined();
+    expect(debugElement.query(By.css("div.username")).nativeElement.textContent).toContain('@');
+    expect(debugElement.query(By.css("div.description")).nativeElement.textContent.trim()).toBe('');
+
+    component.accountStatistics = new AccountStatistics('jsmith', 'John Smith');
+    fixture.detectChanges();
+    expect(component.accountStatistics.username).toBe('jsmith');
+    expect(component.accountStatistics.description).toBe('John Smith');
+    expect(debugElement.query(By.css("div.username")).nativeElement.textContent).toContain('@jsmith');
+    expect(debugElement.query(By.css("div.description")).nativeElement.textContent).toContain('John Smith');
+  });
 });
