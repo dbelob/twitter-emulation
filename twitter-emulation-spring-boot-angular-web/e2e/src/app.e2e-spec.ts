0230aef5bb56a117d9d383fdb424728641b23b60
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-06-30 00:51:16 +0300

    Add e2e tests

--- twitter-emulation-spring-boot-angular-web/e2e/src/app.e2e-spec.ts
+++ twitter-emulation-spring-boot-angular-web/e2e/src/app.e2e-spec.ts
@@ -1,8 +1,19 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
+import { IHttpBackendService } from 'angular';
+import 'angular-mocks';
 
 describe('workspace-project App', () => {
+  let $httpBackend: IHttpBackendService;
   let page: AppPage;
+
+  beforeEach(inject((
+    _$httpBackend_: IHttpBackendService,
+  ) => {
+    $httpBackend = _$httpBackend_;
+
+    $httpBackend.expectGET('api/authentication/user').respond(200, []);
+  }));
 
   beforeEach(() => {
     page = new AppPage();
