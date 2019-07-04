1d36869e0e353851812cb62897e846498adcf101
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-07-04 23:01:51 +0300

    Add Spring Boot Admin support

---
+++ twitter-emulation-spring-boot-admin/src/main/java/acme/twitteremulation/twitteremulationspringbootadmin/TwitterEmulationSpringBootAdminApplication.java
@@ -0,0 +1,13 @@
+package acme.twitteremulation.twitteremulationspringbootadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterEmulationSpringBootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterEmulationSpringBootAdminApplication.class, args);
+	}
+
+}
---
+++ twitter-emulation-spring-boot-admin/src/test/java/acme/twitteremulation/twitteremulationspringbootadmin/TwitterEmulationSpringBootAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package acme.twitteremulation.twitteremulationspringbootadmin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TwitterEmulationSpringBootAdminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
