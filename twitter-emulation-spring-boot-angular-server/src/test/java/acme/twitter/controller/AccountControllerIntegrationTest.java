a344bd330bf9c68fe5989503d7dfaea64768cea3
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2019-07-14 21:12:32 +0300

    Change integration tests for application

--- twitter-emulation-spring-boot-angular-server/src/test/java/acme/twitter/controller/AccountControllerIntegrationTest.java
+++ twitter-emulation-spring-boot-angular-server/src/test/java/acme/twitter/controller/AccountControllerIntegrationTest.java
@@ -6,7 +6,6 @@ import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -31,7 +30,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         classes = App.class)
 @AutoConfigureMockMvc
-@AutoConfigureTestDatabase
 public class AccountControllerIntegrationTest {
     private final String CSRF_COOKIE_NAME = "XSRF-TOKEN";
 
@@ -43,8 +41,8 @@ public class AccountControllerIntegrationTest {
 
     @After
     public void tearDown() throws SQLException {
-        TestUtils.executeSqlScript(dataSource.getConnection(), "/clean.sql");
-        TestUtils.executeSqlScript(dataSource.getConnection(), "/data.sql");
+        TestUtils.executeSqlScript(dataSource.getConnection(), "/clean-h2.sql");
+        TestUtils.executeSqlScript(dataSource.getConnection(), "/data-h2.sql");
     }
 
     @Test
