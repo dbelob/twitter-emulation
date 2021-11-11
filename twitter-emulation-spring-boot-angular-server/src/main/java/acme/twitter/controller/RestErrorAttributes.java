929e3967538a74df16939f678012d6766464d6cd
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2021-11-11 21:13:24 +0300

    Fix warnings

--- twitter-emulation-spring-boot-angular-server/src/main/java/acme/twitter/controller/RestErrorAttributes.java
+++ twitter-emulation-spring-boot-angular-server/src/main/java/acme/twitter/controller/RestErrorAttributes.java
@@ -3,19 +3,20 @@ package acme.twitter.controller;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.Map;
 
 @Component
 public class RestErrorAttributes extends DefaultErrorAttributes {
-    public static String CUSTOM_MESSAGE_ATTRIBUTE = "customMessage";
+    public static final String CUSTOM_MESSAGE_ATTRIBUTE = "customMessage";
 
     @Override
     public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
         Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, options);
 
-        Object customMessage = webRequest.getAttribute(CUSTOM_MESSAGE_ATTRIBUTE, WebRequest.SCOPE_REQUEST);
+        Object customMessage = webRequest.getAttribute(CUSTOM_MESSAGE_ATTRIBUTE, RequestAttributes.SCOPE_REQUEST);
         if (customMessage instanceof String) {
             errorAttributes.put(CUSTOM_MESSAGE_ATTRIBUTE, customMessage);
         }
