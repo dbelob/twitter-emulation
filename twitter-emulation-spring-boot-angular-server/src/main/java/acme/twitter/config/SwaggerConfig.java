d5b0f277ad01c46810212faf708971746521b650
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2021-11-25 20:27:24 +0300

    Fix warnings

--- twitter-emulation-spring-boot-angular-server/src/main/java/acme/twitter/config/SwaggerConfig.java
+++ twitter-emulation-spring-boot-angular-server/src/main/java/acme/twitter/config/SwaggerConfig.java
@@ -2,11 +2,8 @@ package acme.twitter.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springdoc.core.SpringDocUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.security.Principal;
 
 @Configuration
 public class SwaggerConfig {
