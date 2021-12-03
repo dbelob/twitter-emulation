080886580f1fca3dbb9a9a7e35eb830082511cd9
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2021-12-03 18:53:48 +0300

    Add tests

---
+++ twitter-emulation-spring-boot-angular-server/src/test/java/acme/twitter/AppTest.java
@@ -0,0 +1,17 @@
+package acme.twitter;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class AppTest {
+    @Test
+    void contextLoad() {
+        assertTrue(true);
+    }
+}
