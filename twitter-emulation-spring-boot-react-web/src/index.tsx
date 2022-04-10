5bb1f37e005cb6874005812e905c777eaa5cf4cb
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2022-04-11 00:18:23 +0300

    Change styles

--- twitter-emulation-spring-boot-react-web/src/index.tsx
+++ twitter-emulation-spring-boot-react-web/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
