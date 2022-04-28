5dd9de858efaa6b5289ef8f744451f0eff3c4490
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2022-04-28 20:43:31 +0300

    Switch to createRoot in React 18

--- twitter-emulation-spring-boot-react-web/src/index.tsx
+++ twitter-emulation-spring-boot-react-web/src/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
     <React.StrictMode>
         <BrowserRouter>
             <App/>
         </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
