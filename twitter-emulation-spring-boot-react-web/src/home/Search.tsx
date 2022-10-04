becdf2a416452d80e6a2dbbe248ceb0697bf85f0
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2022-10-04 20:18:32 +0300

    Fill search page

--- twitter-emulation-spring-boot-react-web/src/home/Search.tsx
+++ twitter-emulation-spring-boot-react-web/src/home/Search.tsx
@@ -1,12 +1,34 @@
 import { Component } from 'react';
+import { resolve } from 'inversify-react';
+import { Account } from '../common/models/Account';
+import { AccountDataSource } from '../common/datasources/AccountDataSource';
+import Home from "./Home";
+import AccountList from "./AccountList";
 
-export default class Search extends Component {
+type SearchProps = {};
+
+type SearchState = {
+    accounts: Account[];
+};
+
+export default class Search extends Component<SearchProps, SearchState> {
+    @resolve(AccountDataSource)
+    private readonly accountDataSource!: AccountDataSource;
+
+    constructor(props: SearchProps) {
+        super(props);
+
+        this.state = {
+            accounts: []
+        };
+    }
+
     // TODO: implement
     render() {
         return (
-            <>
-                Search
-            </>
+            <Home>
+                <AccountList title={'Search Result'} accounts={this.state.accounts}></AccountList>
+            </Home>
         );
     }
 }
