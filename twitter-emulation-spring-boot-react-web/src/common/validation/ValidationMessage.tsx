4b3eb292f4306b0fd0bdbcbdb9731412440ed18c
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2022-10-24 21:10:30 +0300

    Implement validation

---
+++ twitter-emulation-spring-boot-react-web/src/common/validation/ValidationMessage.tsx
@@ -0,0 +1,22 @@
+import React, { Component, ContextType } from 'react';
+import { ValidationContext } from './ValidationContext';
+
+type ValidationMessageProps = {
+    field: string;
+}
+
+type ValidationMessageState = {}
+
+export class ValidationMessage extends Component<ValidationMessageProps, ValidationMessageState> {
+    static contextType = ValidationContext;
+    declare context: ContextType<typeof ValidationContext>;
+
+    render() {
+        return this.context.getMessagesForField(this.props.field).map(err =>
+            <div className="small bg-danger text-white mt-1 p-1"
+                 key={err}>
+                {err}
+            </div>
+        )
+    }
+}
--- twitter-emulation-spring-boot-react-web/src/registration/Registration.tsx
+++ twitter-emulation-spring-boot-react-web/src/registration/Registration.tsx
@@ -1,15 +1,39 @@
 import { ChangeEvent, Component } from 'react';
+import { FormValidator } from '../common/validation/FormValidator';
+import { ValidateForm } from '../common/validation/WholeFormValidateForm';
+import { ValidationMessage } from '../common/validation/ValidationMessage';
 
-type RegistrationProps = {};
+type RegistrationProps = {
+};
 
 type RegistrationState = {
-    username: '';
-    password: '';
-    passwordConfirmation: '';
-    description: '';
+    username: string;
+    password: string;
+    passwordConfirmation: string;
+    description: string;
 };
 
 export default class Registration extends Component<RegistrationProps, RegistrationState> {
+    private rules: any;
+
+    constructor(props: RegistrationProps) {
+        super(props);
+
+        this.state = {
+            username: '',
+            password: '',
+            passwordConfirmation: '',
+            description: ''
+        };
+
+        this.rules = {
+            username: {required: true, minlength: 1, maxlength: 16},
+            password: {required: true, minlength: 5, maxlength: 25, equals: 'passwordConfirmation'},
+            passwordConfirmation: {required: true, minlength: 5, maxlength: 25, equals: 'password'},
+            description: {required: true, minlength: 2, maxlength: 30}
+        };
+    }
+
     updateFormValue = (event: ChangeEvent<HTMLInputElement>) => {
         const name = event.target.name;
         const value = event.target.value;
@@ -20,40 +44,55 @@ export default class Registration extends Component<RegistrationProps, Registrat
         }));
     }
 
+    submit = (data: any) => {
+        // TODO: implement
+        console.log('Registration data: ' + JSON.stringify(data));
+    }
+
     render() {
         return (
             <div className="container-dialog p-2">
                 <h3 className="bg-info p-1 text-white text-center rounded">Registration</h3>
 
-                <div className="mb-3">
-                    <label htmlFor="username" className="form-label">Username:</label>
-                    <input type="text"
-                           className="form-control" id="username" name="username"
-                           value={this.state.username}
-                           onChange={this.updateFormValue}
-                           autoFocus/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password:</label>
-                    <input type="password"
-                           className="form-control" id="password" name="password"
-                           value={this.state.password}
-                           onChange={this.updateFormValue}/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="confirmation" className="form-label">Confirmation:</label>
-                    <input type="password"
-                           className="form-control" id="confirmation" name="confirmation"
-                           value={this.state.passwordConfirmation}
-                           onChange={this.updateFormValue}/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="description" className="form-label">Description:</label>
-                    <input type="text"
-                           className="form-control" id="description" name="description"
-                           value={this.state.description}
-                           onChange={this.updateFormValue}/>
-                </div>
+                <FormValidator data={this.state} rules={this.rules}
+                               submit={this.submit}
+                               validateForm={ValidateForm}>
+                    <ValidationMessage field="form"/>
+                    
+                    <div className="mb-3">
+                        <label htmlFor="username" className="form-label">Username:</label>
+                        <input type="text"
+                               className="form-control" id="username" name="username"
+                               value={this.state.username}
+                               onChange={this.updateFormValue}
+                               autoFocus/>
+                        <ValidationMessage field="username"/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password" className="form-label">Password:</label>
+                        <input type="password"
+                               className="form-control" id="password" name="password"
+                               value={this.state.password}
+                               onChange={this.updateFormValue}/>
+                        <ValidationMessage field="password"/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="confirmation" className="form-label">Confirmation:</label>
+                        <input type="password"
+                               className="form-control" id="confirmation" name="confirmation"
+                               value={this.state.passwordConfirmation}
+                               onChange={this.updateFormValue}/>
+                        <ValidationMessage field="confirmation"/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="description" className="form-label">Description:</label>
+                        <input type="text"
+                               className="form-control" id="description" name="description"
+                               value={this.state.description}
+                               onChange={this.updateFormValue}/>
+                        <ValidationMessage field="description"/>
+                    </div>
+                </FormValidator>
             </div>
         );
     }
