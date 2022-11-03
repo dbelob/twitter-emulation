d634ff9095a29c0d51a148810a7d00b2b0a80e88
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2022-11-03 20:01:08 +0300

    Simplify component

--- twitter-emulation-spring-boot-react-web/src/profile/DeleteAccount.tsx
+++ twitter-emulation-spring-boot-react-web/src/profile/DeleteAccount.tsx
@@ -1,75 +1,51 @@
-import React, { Component } from 'react';
-import { Link, Navigate } from 'react-router-dom';
-import { resolve } from 'inversify-react';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useInjection } from 'inversify-react';
 import { AccountService } from '../common/services/AccountService';
 import { AuthenticationService } from '../common/services/AuthenticationService';
 import { User } from '../common/models/User';
 import MessageText from '../message/MessageText';
 
-type DeleteAccountProps = {};
+export default function DeleteAccount() {
+    const [user, setUser] = useState<User>(new User());
+    const authenticationService = useInjection(AuthenticationService);
+    const accountService = useInjection(AccountService);
+    const navigate = useNavigate();
 
-type DeleteAccountState = {
-    user: User,
-    isSubmit: boolean;
-}
-
-export default class DeleteAccount extends Component<DeleteAccountProps, DeleteAccountState> {
-    @resolve(AuthenticationService)
-    private readonly authenticationService!: AuthenticationService;
-
-    @resolve(AccountService)
-    private readonly accountService!: AccountService;
-
-    constructor(props: DeleteAccountProps) {
-        super(props);
-
-        this.state = {
-            user: new User(),
-            isSubmit: false
-        };
-    }
-
-    componentDidMount() {
-        this.authenticationService.getUser()
+    useEffect(() => {
+        authenticationService.getUser()
             .subscribe(data => {
-                this.setState({
-                    user: data
-                });
+                setUser(data);
             });
-    }
+    }, []);
 
-    delete = () => {
-        if (this.state.user.name) {
-            this.accountService.deleteAccount(this.state.user.name)
-                .subscribe(data => {
-                    this.setState({
-                        isSubmit: true
+    function deleteAccount() {
+        if (user.name) {
+            accountService.deleteAccount(user.name)
+                .subscribe(() => {
+                    navigate({
+                        pathname: '/login',
+                        search: '?logout=1'
                     });
                 });
         }
     }
 
-    render() {
-        return (
-            <>
-                {this.state.isSubmit ? <Navigate to="/login?logout=1"/> :
-                    <div className="container-dialog p-2">
-                        <h3 className="bg-danger p-1 text-white text-center rounded">Delete account</h3>
+    return (
+        <div className="container-dialog p-2">
+            <h3 className="bg-danger p-1 text-white text-center rounded">Delete account</h3>
 
-                        <MessageText/>
+            <MessageText/>
 
-                        <div className="alert text-center">
-                            Are you sure to delete account '{this.state.user.name}'?
-                        </div>
-                        <div className="text-center">
-                            <button className="btn btn-danger m-1" onClick={this.delete}>Delete</button>
-                            <Link to="/account/profile">
-                                <button className="btn btn-secondary m-1">Cancel</button>
-                            </Link>
-                        </div>
-                    </div>
-                }
-            </>
-        );
-    }
+            <div className="alert text-center">
+                Are you sure to delete account '{user.name}'?
+            </div>
+            <div className="text-center">
+                <button className="btn btn-danger m-1" onClick={deleteAccount}>Delete</button>
+                <Link to="/account/profile">
+                    <button className="btn btn-secondary m-1">Cancel</button>
+                </Link>
+            </div>
+        </div>
+    );
 }
