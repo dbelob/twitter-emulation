d418e46bc69bbcffd1a8579384e26065a56d8eaf
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2022-11-03 20:29:20 +0300

    Add non validation message component

--- twitter-emulation-spring-boot-angular-web/src/app/modules/message/message.module.ts
+++ twitter-emulation-spring-boot-angular-web/src/app/modules/message/message.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MessageComponent } from "./message.component";
 import { MessageService } from "./message.service";
+import { NonValidationMessageComponent } from './non-validation-message.component';
 
 @NgModule({
-  declarations: [MessageComponent],
+  declarations: [MessageComponent, NonValidationMessageComponent],
   imports: [CommonModule],
   providers: [MessageService],
-  exports: [MessageComponent]
+  exports: [MessageComponent, NonValidationMessageComponent]
 })
 export class MessageModule {
 }
---
+++ twitter-emulation-spring-boot-angular-web/src/app/modules/message/non-validation-message.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-non-validation-message',
+  templateUrl: './non-validation-message.component.html'
+})
+export class NonValidationMessageComponent {
+  @Input() public isFormSubmitted: boolean;
+  @Input() public isFormValid: boolean;
+
+  constructor() {
+  }
+}
