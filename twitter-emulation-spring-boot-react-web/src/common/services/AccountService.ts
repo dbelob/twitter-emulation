22f013122b4fa8e3e384f2e8e75812b347f00af4
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2022-11-04 19:22:25 +0300

    Implement top bar and search components

--- twitter-emulation-spring-boot-react-web/src/common/services/AccountService.ts
+++ twitter-emulation-spring-boot-react-web/src/common/services/AccountService.ts
@@ -1,4 +1,4 @@
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { AxiosError } from 'axios';
 import { Axios, AxiosObservable } from 'axios-observable';
@@ -55,9 +55,21 @@ export class AccountService {
             );
     }
 
-    getAccounts(usernamePart: string): Observable<Account[]> {
-        // TODO: implement
-        return of([]);
+    getAccounts(usernamePart: string | null | undefined): Observable<Account[]> {
+        const config = usernamePart ? {
+            params: {
+                'usernamePart': usernamePart
+            }
+        } : {};
+
+        return Axios.get<Account[]>(`${this.baseUrl}/accounts`, config)
+            .pipe(
+                map(response => response.data),
+                catchError((err: AxiosError) => {
+                    this.messageService.reportMessage(err.response);
+                    throw err;
+                })
+            );
     }
 
     getAccountStatistics(username: string): Observable<AccountStatistics> {
