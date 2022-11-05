f853f0c75085519e4de3d71c810ba7129bfebb76
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2022-11-05 20:13:09 +0300

    Implement status info component

--- twitter-emulation-spring-boot-react-web/src/home/StatusInfo.tsx
+++ twitter-emulation-spring-boot-react-web/src/home/StatusInfo.tsx
@@ -14,24 +14,21 @@ export default class StatusInfo extends Component<StatusInfoProps, StatusInfoSta
     constructor(props: StatusInfoProps) {
         super(props);
 
-        // TODO: change
         this.state = {
             copyrightDate: new Date()
         };
     }
 
-    isStateVisible(): boolean {
-        return this.props.userState.isAuthenticated();
-    }
-
     render() {
         return (
             <div className="card p-3 bg-light">
-                <div className="row m-1" id="state">
-                    <div className="col">
-                        Logged in as <span className="fw-bold">{this.props.userState.authenticatedUserName}</span>
+                {this.props.userState.isAuthenticated() &&
+                    <div className="row m-1" id="state">
+                        <div className="col">
+                            Logged in as <span className="fw-bold">{this.props.userState.authenticatedUserName}</span>
+                        </div>
                     </div>
-                </div>
+                }
                 <div className="row m-1">
                     <div className="col">
                         &copy; Acme, {this.state.copyrightDate && format(this.state.copyrightDate, 'yyyy')}
