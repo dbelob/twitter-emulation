5c84984860f1bd28bbba3619fa4bb9fb2d6b0232
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2024-08-27 18:33:05 +0300

    Fix DI

--- twitter-emulation-spring-boot-react-web2/vite.config.ts
+++ twitter-emulation-spring-boot-react-web2/vite.config.ts
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      parserOpts: {
+        plugins: ['decorators-legacy'],
+      },
+    },
+  })],
   build: {
     outDir: 'target'
   },
