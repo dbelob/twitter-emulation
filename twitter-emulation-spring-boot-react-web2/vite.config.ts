ce36c7d85fe3c57cc3c7e6fdc7dd61efce4aeccd
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2024-08-29 19:58:42 +0300

    Support tests

--- twitter-emulation-spring-boot-react-web2/vite.config.ts
+++ twitter-emulation-spring-boot-react-web2/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -21,5 +22,10 @@ export default defineConfig({
         secure: false
       }
     }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['src/setupTests.ts']
   }
 })
