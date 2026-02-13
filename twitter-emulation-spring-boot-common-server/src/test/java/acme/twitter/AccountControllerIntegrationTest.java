5157dc7c38ea4fb67f202631c97f0a7caf6622ea
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2026-02-13 12:49:08 +0300

    Use tests after Spring Boot Admin 4 release

--- twitter-emulation-spring-boot-common-server/src/test/java/acme/twitter/AccountControllerIntegrationTest.java
+++ twitter-emulation-spring-boot-common-server/src/test/java/acme/twitter/AccountControllerIntegrationTest.java
@@ -30,7 +30,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         classes = App.class)
 @AutoConfigureMockMvc
 @ActiveProfiles("development")
-@Disabled("Fix it after Spring Boot Admin 4 release")
 class AccountControllerIntegrationTest {
     @Autowired
     private MockMvc mvc;
