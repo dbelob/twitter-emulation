3188c53afa8f1acad41ce97e2bbfd9417212537a
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2026-03-14 22:59:40 +0300

    Fix build error

--- twitter-emulation-spring-boot-react-web/vite.config.ts
+++ twitter-emulation-spring-boot-react-web/vite.config.ts
@@ -3,13 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    babel: {
-      parserOpts: {
-        plugins: ['decorators-legacy'],
-      },
-    },
-  })],
+  plugins: [react()],
   build: {
     outDir: 'target/classes/static'
   },
