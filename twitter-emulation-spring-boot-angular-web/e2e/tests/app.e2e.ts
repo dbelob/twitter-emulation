26490dcefc2e2396bf3523796fad9426ff37d075
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2026-07-30 19:40:43 +0300

    Add e2e for Angular

--- twitter-emulation-spring-boot-angular-web/e2e/tests/app.e2e.ts
+++ twitter-emulation-spring-boot-angular-web/e2e/tests/app.e2e.ts
@@ -1,12 +1,27 @@
 import {setupBrowserHooks, getBrowserState} from './utils';
+import { getPageHeaderText, getPageTitleText, navigateTo } from './app.po';
 
 describe('App test', function () {
   setupBrowserHooks();
 
   it('is running', async function () {
     const {page} = getBrowserState();
-    const element = await page.locator('::-p-text(Log in)').wait();
+    const element = await page.locator('::-p-text(Login)').wait();
 
     expect(element).not.toBeNull();
+  });
+
+  it('should open application', async () => {
+    const {page, baseUrl} = getBrowserState();
+
+    await navigateTo(page, baseUrl);
+    expect(await getPageTitleText(page)).toBe('Twitter (Angular)');
+  });
+
+  it('should display login page', async () => {
+    const {page, baseUrl} = getBrowserState();
+
+    await navigateTo(page, baseUrl);
+    expect(await getPageHeaderText(page)).toBe('Login');
   });
 });
