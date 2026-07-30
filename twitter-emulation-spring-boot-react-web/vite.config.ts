67d11743abf90ca461181e908e90771018846d59
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2026-07-30 13:39:35 +0300

    Exclude e2e from unit test for React

--- twitter-emulation-spring-boot-react-web/vite.config.ts
+++ twitter-emulation-spring-boot-react-web/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config';
+import { configDefaults, defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -19,6 +19,10 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['src/setupTests.ts']
+    setupFiles: ['src/setupTests.ts'],
+    exclude: [
+        ...configDefaults.exclude,
+        'e2e/**'
+    ]
   }
 })
