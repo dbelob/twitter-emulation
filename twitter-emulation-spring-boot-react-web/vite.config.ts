105376aee795910d0f8aa73c3b45835c9187f564
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2026-07-30 15:48:34 +0300

    Simplify test and e2e for React

--- twitter-emulation-spring-boot-react-web/vite.config.ts
+++ twitter-emulation-spring-boot-react-web/vite.config.ts
@@ -1,4 +1,4 @@
-import { configDefaults, defineConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -19,10 +19,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['src/setupTests.ts'],
-    exclude: [
-        ...configDefaults.exclude,
-        'e2e/**'
-    ]
+    setupFiles: ['src/setupTests.ts']
   }
 })
---
+++ twitter-emulation-spring-boot-react-web/vitest.config.ts
@@ -0,0 +1,14 @@
+import { configDefaults, defineConfig, mergeConfig } from 'vitest/config';
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+    exclude: [
+        ...configDefaults.exclude,
+        'e2e/**'
+      ]
+    }
+  })
+);
